cb2c513844dd14c610ae6a9bebda331f58bf1a62
jshah98 <jinanshshah98@gmail.com>
2021-02-11 21:15:01 -0500

    robo_factor = 2 dest_factor = 3 robo_dest_factor = 1

--- solution.py
+++ solution.py
@@ -52,8 +52,8 @@ def heur_alternate(state):
     #Write a heuristic function that improves upon heur_manhattan_distance to estimate distance between the current state and the goal.
     #Your function should return a numeric value for the estimate of the distance to the goal.
 
-    robo_factor = 0.5
-    dest_factor = 1
+    robo_factor = 2
+    dest_factor = 3
     robo_dest_factor = 1
 
     h = 0
